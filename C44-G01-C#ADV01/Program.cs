0cbf627005b6afacf418812291fe20278a264375
Abdullah Hasan <AbdullahAbdelhady929@gmail.com>
2025-08-15 07:55:48 +0300

    Range T

--- C44-G01-C#ADV01/Program.cs
+++ C44-G01-C#ADV01/Program.cs
@@ -35,7 +35,43 @@
                     if (!swapped) break;
                 }
                 #endregion
+                #region Range T 
+                public class Range<T> where T : IComparable<T>
+            {
+                public T Minimum { get; }
+                public T Maximum { get; }
+
+                public Range(T min, T max)
+                {
+                    if (min.CompareTo(max) > 0)
+                        throw new ArgumentException("Minimum cannot be greater than maximum");
+
+                    Minimum = min;
+                    Maximum = max;
+                }
+
+                public bool IsInRange(T value)
+                {
+                    return value.CompareTo(Minimum) >= 0 && value.CompareTo(Maximum) <= 0;
+                }
+
+                public T Length()
+                {
+                    // For numeric types, we'd need to use dynamic or other approaches
+                    // This simple implementation assumes T supports subtraction
+                    // A more complete solution would require additional constraints
+                    dynamic min = Minimum;
+                    dynamic max = Maximum;
+                    return max - min;
+                }
+
+                public override string ToString()
+                {
+                    return $"[{Minimum} - {Maximum}]";
+                }
             }
+            #endregion 
         }
     }
-}
+    }
+
