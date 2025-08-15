5697ca74bd7acf863409b6e1330a36def4a9069f
Abdullah Hasan <AbdullahAbdelhady929@gmail.com>
2025-08-15 07:53:41 +0300

    SortingAlgorithms

--- C44-G01-C#ADV01/Program.cs
+++ C44-G01-C#ADV01/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           
+        }
+        #region SortingAlgorithms
+        public class SortingAlgorithms
+        {
+            public static void OptimizedBubbleSort<T>(T[] array) where T : IComparable<T>
+            {
+                bool swapped;
+                int n = array.Length;
+
+                for (int i = 0; i < n - 1; i++)
+                {
+                    swapped = false;
+
+                    // Last i elements are already in place
+                    for (int j = 0; j < n - i - 1; j++)
+                    {
+                        if (array[j].CompareTo(array[j + 1]) > 0)
+                        {
+                            // Swap elements
+                            T temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+                            swapped = true;
+                        }
+                    }
+
+                    // If no swaps occurred, array is sorted
+                    if (!swapped) break;
+                }
+                #endregion
+            }
         }
     }
 }
